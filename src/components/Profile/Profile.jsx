c87b87d8f767f24d2b2458dcc92b919b1b9ffbd9
giannencljsn <giannejuson16@gmail.com>
2024-09-02 16:10:20 +0800

    +profile desc, buttons

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from "react";
 
-import styles from './Profile.module.css';
-import { getImageUrl } from '../../utils'
-
+import styles from "./Profile.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Profile = () => {
   return (
     <section className={styles.container}>
-        <div className={styles.content}> 
-            <h1 className={styles.title}>Hello, this is Gianne</h1>
-            <p className={styles.description}>I'm a full-stack developer with 1 year of experience using Javascript, PHP, Codeigniter, MYSQL.
-                Feel free to contact me for any inquiries. </p>
-            <a href="mailto:giannejuson16@gmail.com" className={styles.contactBtn}>Message Me!</a>
-        </div>
-        <img src={getImageUrl("profile/profileImage.png")} alt="Gianne profile image" />
-            <div className={styleSheet.topBlur}></div>
-            <div className={styleSheet.bottomBlur}></div>
-
-   
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hello, this is Gianne</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with 1 year of experience in Javascript, PHP, Codeigniter, MYSQL, feel free to contact me for more information!
+        </p>
+        <button className={styles.contactBtn}>
+        <span><a href="mailto:giannejuson16@gmail.com" className={styles.anchorCbtn}>Send a Message</a></span>
+        </button>
+        
+      </div>
+      <img src={getImageUrl("profile/profileImage.png")} alt="Gianne profile image" className={styles.heroImg}/>
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
     </section>
-  )
-}
+  );
+};
