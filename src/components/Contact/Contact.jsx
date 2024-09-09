c5f1e0f1f74f2f261bb831f258c60bffa82acd00
giannencljsn <giannejuson16@gmail.com>
2024-09-09 18:39:14 +0800

    +contact info

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,24 +7,24 @@ export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <h2>Contact</h2>
-        <p>Feel free to reach out!</p>
+        <h2>Contact Me</h2>
+        <p>Leave a message!</p>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:giannejuson16@gmail.com">giannejuson16@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/gianne-nicole-juson/">linkedin.com/in/gianne-nicole-juson/</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://github.com/giannencljsn">github.com/giannencljsn</a>
         </li>
       </ul>
     </footer>
